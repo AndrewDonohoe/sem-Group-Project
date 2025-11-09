872e3fd4b52f34db932cb77ca8a5853ccfb27e75
Romasav <roma.savitski@gmail.com>
2025-11-09 11:44:44 +0000

    create CountryLanguage

---
+++ src/main/java/com/napier/sem/models/CountryLanguage.java
@@ -0,0 +1,33 @@
+package com.napier.sem.models;
+
+public class CountryLanguage {
+
+    private final String countyCode;
+    private final String language;
+    private final Boolean isOfficial;
+    private final Float percentage;
+
+    public CountryLanguage(String countryCode, String language, Boolean isOfficial, Float percentage) {
+        this.countyCode = countryCode;
+        this.language = language;
+        this.isOfficial = isOfficial;
+        this.percentage = percentage;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public Boolean getIsOfficial() {
+        return isOfficial;
+    }
+
+    public Float getPercentage() {
+        return percentage;
+    }
+
+}
