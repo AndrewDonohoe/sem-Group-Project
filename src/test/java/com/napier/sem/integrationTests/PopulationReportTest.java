c3dc118f4859ae73a075a3b1e3b659019933f678
AndrewDonohoe <40719855@live.napier.ac.uk>
2025-11-11 13:21:59 +0000

    Implemented the PopulationReport integration tests

--- src/test/java/com/napier/sem/integrationTests/PopulationReportTest.java
+++ src/test/java/com/napier/sem/integrationTests/PopulationReportTest.java
@@ -2,8 +2,12 @@ package com.napier.sem.integrationTests;
 
 import com.napier.sem.db.Database;
 import com.napier.sem.db.DatabaseInterface;
+import com.napier.sem.reports.PopulationReport;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PopulationReportTest {
 
@@ -20,35 +24,81 @@ public class PopulationReportTest {
 
     @Test
     public void testPopulationOfPeopleInAndOutOfCitiesInEachContinent() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfPeopleInAndOutOfCitiesInEachContinent();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testPopulationOfPeopleInAndOutOfCitiesInEachRegion() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result =  populationReport.populationOfPeopleInAndOutOfCitiesInEachRegion();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testPopulationOfPeopleInAndOutOfCitiesInEachCountry() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfPeopleInAndOutOfCitiesInEachCountry();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
-    public void testPopulationOfArea() {
-//        if (area.equals("World")) {
-//            System.out.println("Area is World");
-//        } else if (area.equals("Continent")) {
-//
-//        } else if (area.equals("Region")) {
-//
-//        } else if (area.equals("Country")) {
-//
-//        } else if (area.equals("District")) {
-//
-//        } else if (area.equals("City")) {
-//
-//        }
+    public void testPopulationOfWorld() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfArea("World");
+        assertNotNull(result);
+        assertNotEquals("", result);
+    }
+
+    @Test
+    public void testPopulationOfContinent() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result  = populationReport.populationOfArea("Continent");
+        assertNotNull(result);
+        assertNotEquals("", result);
+    }
+
+    @Test
+    public void testPopulationOfRegion() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfArea("Region");
+        assertNotNull(result);
+        assertNotEquals("", result);
+    }
+
+    @Test
+    public void testPopulationOfCountry() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfArea("Country");
+        assertNotNull(result);
+        assertNotEquals("", result);
+    }
+
+    @Test
+    public void testPopulationOfDistrict() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfArea("District");
+        assertNotNull(result);
+        assertNotEquals("", result);
+    }
+
+    @Test
+    public void testPopulationOfCity() {
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfArea("City");
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testPopulationOfPeopleWhoSpeakDifferentLanguages() {
-
+        PopulationReport populationReport = new PopulationReport(db);
+        String result = populationReport.populationOfPeopleWhoSpeakDifferentLanguages();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 }
