141e3002caf1f0f1dddac4958cafe6d7f7fcc77e
AndrewDonohoe <40719855@live.napier.ac.uk>
2025-11-11 13:12:51 +0000

    Implemented the CountryReport integration tests

--- src/test/java/com/napier/sem/integrationTests/CountryReportTest.java
+++ src/test/java/com/napier/sem/integrationTests/CountryReportTest.java
@@ -2,8 +2,12 @@ package com.napier.sem.integrationTests;
 
 import com.napier.sem.db.Database;
 import com.napier.sem.db.DatabaseInterface;
+import com.napier.sem.reports.CountryReport;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CountryReportTest {
 
@@ -20,31 +24,49 @@ public class CountryReportTest {
 
     @Test
     public void testCountriesInWorldLargestToSmallest() {
-
+        CountryReport countryReport = new CountryReport(db);
+        String result = countryReport.countriesInWorldLargestToSmallest();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testCountriesInContinentSmallestToLargest() {
-
+        CountryReport countryReport = new CountryReport(db);
+        String result = countryReport.countriesInContinentSmallestToLargest();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testCountriesInRegionSmallestToLargest() {
-
+        CountryReport countryReport = new CountryReport(db);
+        String result = countryReport.countriesInRegionSmallestToLargest();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testTopPopulatedCountriesInWorld() {
-
+        CountryReport countryReport = new CountryReport(db);
+        String result = countryReport.topPopulatedCountriesInWorld(10);
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testTopPopulatedCountriesInContinent() {
-
+        CountryReport countryReport = new CountryReport(db);
+        String result = countryReport.topPopulatedCountriesInContinent(10);
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testTopPopulatedCountriesInRegion() {
-
+        CountryReport countryReport = new CountryReport(db);
+        String result = countryReport.topPopulatedCountriesInRegion(10);
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 }
