2d08045a8a741d0e00ed2f453744b08318fcac90
williammerry2310 <40722676@live.napier.ac.uk>
2025-11-17 14:20:24 +0000

    Implemented my usecase8 query

--- src/main/java/com/napier/sem/db/SqlQueries.java
+++ src/main/java/com/napier/sem/db/SqlQueries.java
@@ -62,6 +62,23 @@ public class SqlQueries {
     ORDER BY total_population DESC;
     """;
 
+
+    public static String populationOfPeopleWhoSpeakDifferentLanguagesQuery = """
+            SELECT  cl.Language,
+                    ROUND (SUM(c.Population * (cl.Percentage / 100.0))) AS speakers,
+                ROUND(100.0 * SUM(c.Population * (cl.Percentage / 100.0)) /
+                (SELECT SUM(Population)
+                FROM country),2) AS pct_of_world
+            FROM countrylanguage cl
+            JOIN country c  ON c.Code = cl.CountryCode
+            WHERE cl.Language IN ('Chinese','English','Hindi','Spanish','Arabic')
+            GROUP BY cl.Language
+            ORDER BY speakers DESC;
+    """;
+
+
+
+
     // ----------------------------------------
     // Queries linked with use-case 1
     // ----------------------------------------
