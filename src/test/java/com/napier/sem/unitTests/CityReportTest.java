4a75b8301e5b08c00bf1956e5850919982f91b9c
Tyler Smith <40718506@live.napier.ac.uk>
2025-11-23 12:56:35 +0000

    unit tests added

--- src/test/java/com/napier/sem/unitTests/CityReportTest.java
+++ src/test/java/com/napier/sem/unitTests/CityReportTest.java
@@ -1,26 +1,54 @@
 package com.napier.sem.unitTests;
 
+import com.napier.sem.models.City;
+import com.napier.sem.reports.CityReport;
 import org.junit.jupiter.api.Test;
 
 public class CityReportTest {
     @Test
     public void testCitiesInWorldLargestToSmallest() {
+        CityReport cityReport = new CityReport(db);
+        String result = cityReport.citiesInWorldLargestToSmallest();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testCitiesInContinentLargestToSmallest() {
+        CityReport = cityReport = new CityReport(db);
+        String result = cityReport.citiesInContinentLargestToSmallest();
+        assertNotNull(result);
+        assertNotEquals("", result);
     }
 
     @Test
     public void testCitiesInRegionLargestToSmallest() {
+        {
+            CityReport = cityReport = new CityReport(db);
+            String result = cityReport.citiesInRegionLargestToSmallest();
+            assertNotNull(result);
+            assertNotEquals("", result);
+        }
     }
 
     @Test
     public void testCitiesInCountryLargestToSmallest() {
+        {
+            CityReport = cityReport = new CityReport(db);
+            String result = cityReport.citiesInCountryLargestToSmallest();
+            assertNotNull(result);
+            assertNotEquals("", result);
+        }
     }
 
     @Test
     public void testCitiesInDistrictLargestToSmallest() {
+        {
+            CityReport = cityReport = new CityReport(db);
+            String result = cityReport.citiesInDistrictLargestToSmallest();
+            assertNotNull(result);
+            assertNotEquals("", result);
+        }
     }
 
     @Test
